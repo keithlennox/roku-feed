42e0e2871921ffb49b8415f185f9b4c61be87d62
klennox <klennox@tvo.org>
2021-03-18 14:25:38 -0400

    created separate function files

---
+++ functions/brightcove.js
@@ -0,0 +1,96 @@
+/*Functions used to call Brightcove CMS API to retrieve video metadata*/
+
+const axios = require('axios');
+
+//Get API token (required for all Brightcove API calls)
+const getToken = async () => {
+  const client_id = "2a703469-6009-4204-bcb3-ba3cec61abf5";
+  const client_secret = "4munZY-rUvfs-SnoaMXkkywC_z9Li5fViX_GGOz2k9A-IwkOhEksFiGsdG88g_1JmuYd_60Tvk5wf48Cdvv52g";
+  var auth_string = new Buffer(client_id + ":" + client_secret).toString('base64');
+  const oauth_body = "grant_type=client_credentials";
+  const oauth_options = {
+    headers: {
+      'Authorization': 'Basic ' + auth_string,
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  }
+  let now =  Date.now();
+  if(now > getToken.expireTime || !getToken.expireTime) { //If Brightcove token has expired or is undefined
+    let oauth_result = await axios.post("https://oauth.brightcove.com/v3/access_token", oauth_body, oauth_options)
+    getToken.options = { //Token saved as function parameter so it persists between function calls
+      headers: {
+        'Authorization': 'Bearer ' + oauth_result.data.access_token,
+        'Content-type' : 'application/json'
+      }
+    }
+    getToken.expireTime = Date.now() + 290000; //Now + 290 sec (BC tokens expire in 300 sec / 5 min). Saved as function parameter so it persists between function calls.
+    console.log("Returned new token");
+    return getToken.options; //Return a new token
+  }
+  console.log("Returned exisiting token");
+  return getToken.options; //Return the exisiting token because it has not expired yet
+}
+  
+//Sleep function
+//Used for Brightcove rate limiting and retries
+//Must work with promises and async/await
+const sleep = (ms) => {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+  
+//Get Brightcove video metadata
+//Accepts a Brightcove account id
+//Returns array of objects containing metadata for all videos in account that match the search criteria
+exports.getBrightcoveVideos = async (account) => {
+  console.log("Retrieving videos");
+  let counter = 0; //initialize counter
+  let bcVideos = []; //Create empty videos array
+  //const search = "tags:roku";
+  const search = "ott_flag:true";
+  while(counter === bcVideos.length) { //Get next 100 videos until no more are returned
+    for (i = 1; i <=3; i++) { //Retry on error
+      try{
+        console.log(counter);
+        console.log(bcVideos.length);
+        let options = await getToken();
+        let response = await axios.get("https://cms.api.brightcove.com/v1/accounts/" + account + "/videos?query=" + search + "&limit=100&offset=" + counter, options);
+        bcVideos.push(...response.data);
+        console.log("Get next 100 videos")
+        break; //No need to retry
+      }catch(error){
+        console.error(error);
+        await sleep(5000); //Wait 5 seconds between retries
+      }
+    }//End retry loop
+    await sleep(111); //Brightcove rate limiting = less than 10 requests per second (111 ms = 9 requests per second)
+    counter = counter + 100; //Increment counter
+  }
+  return bcVideos;//This may be an empty array if all API calls fail
+}
+  
+//Get Brightcove sources
+//Accepts an array of Brightcove video objects
+//Returns the same array with the addition of a video URL for each video
+exports.getBrightcoveSource = async (bcVideos) => {
+  console.log("Retrieving sources");
+  for(let bcVideo of bcVideos) {//For every video in the array
+    for (i = 1; i <=3; i++) { //Retry on error
+      try{
+        let options = await getToken();
+        let response = await axios.get("https://cms.api.brightcove.com/v1/accounts/" + bcVideo.account_id + "/videos/" + bcVideo.id + "/sources", options) //Get the sources array
+        for(let source of response.data) { //For each sources array...
+          if(source.src && source.src.startsWith("https://") && source.type === "application/x-mpegURL") { //Get the HLS source
+            console.log(source.src);
+            bcVideo.video_url = source.src;
+          }
+        }
+        break; // No need to retry
+      }catch(error){
+        console.error(error);
+        await sleep(5000); //Wait 5 seconds between retries
+      }
+    }//End retry loop
+    await sleep(111); //Brightcove rate limiting = less than 10 requests per second (111 ms = 9 requests per second)
+  }
+  return bcVideos;
+} //End function
---
+++ functions/roku.js
@@ -0,0 +1,93 @@
+/*Functions used to create various Roku rss feed components
+Errors thrown here bubble up and are caught at higher levels
+*/
+
+//Create Roku caption url
+//Accepts a single Brightcove video object
+//Returns the appropriate caption URL for that video
+const getBrightcoveCaptions = (bcItem) => {
+  if(bcItem.text_tracks.length != 0) {
+    let text_track = bcItem.text_tracks.find((item)=> item.kind === "captions");
+    if(text_track) {
+      let url = text_track.sources.find((item2) => item2.src.startsWith("https://"))
+      if(url) {
+        return url.src;
+      }
+    }
+  }
+  throw new ReferenceError("Caption file not found for video " + bcItem.id);
+}
+
+//Create Roku video object
+//Accepts a Brightcove video object
+//Retruns a Roku video object
+//Note: Generes used for movies and tv shows only. Tags used for movies and tv shows only. EpisodeNumber used for series only.
+exports.createRokuVideo = (bcItem) => {
+  let videoObject = {};
+  videoObject.id = bcItem.reference_id;
+  videoObject.title = bcItem.name;
+  videoObject.content = {};
+  videoObject.content.dateAdded = `${bcItem.custom_fields.ott_release_date}T08:00:00+04:00`; //YYYY-MM-DDTHH:MM:SS+HH:MM. Used to generate the “Recently Added” category. Everything is relased 8 or 9 AM Toronto time.
+  videoObject.content.duration = Math.round(bcItem.duration / 1000); //Brightcove returns miliseconds. Roku requires seconds and must be an integer.
+  videoObject.content.language = "en-us";
+  videoObject.content.validityPeriodStart = bcItem.schedule.starts_at;
+  videoObject.content.validityPeriodEnd = bcItem.schedule.ends_at;
+  videoObject.content.videos = {};
+  videoObject.content.videos.videoType = "HLS";
+  if(bcItem.video_url) {
+    videoObject.content.videos.url = bcItem.video_url;
+  }else {
+    throw new ReferenceError("No video url found for " + bcItem.id);
+  }
+  videoObject.content.videos.quality = "FHD";
+  videoObject.content.captions = {};
+  videoObject.content.captions.language = "en";
+  videoObject.content.captions.captionType = "CLOSED_CAPTION";
+  videoObject.content.captions.url = getBrightcoveCaptions(bcItem);
+  if(bcItem.custom_fields.ott_type === "movies" || bcItem.custom_fields.ott_type === "tv specials") {
+    videoObject.genres = bcItem.custom_fields.ott_genres.trim().replace(/ *, */g, ",").split(","); //Trim whitespace and convert string to array
+    videoObject.tags = bcItem.custom_fields.ott_tags.trim().replace(/ *, */g, ",").split(","); //Trim whitespace and convert string to array
+  }
+  bcItem.images.thumbnail.sources.forEach((source) => {
+    if(source.src.startsWith("https://")) {
+      videoObject.thumbnail = source.src //Should throw error if cc not found
+    }
+  }) 
+  videoObject.releaseDate = bcItem.ott_release_date; //YYYY-MM-DD. Used to sort programs chronologically and group related content in Roku Search.
+  if(bcItem.custom_fields.ott_type === "series with seasons" || bcItem.custom_fields.ott_type === "series without seasons") {
+    if(bcItem.custom_fields.ott_episode_number.match(/^[1-9][0-9]{0,1}$/)) { //Must be a 1 or 2 digit positive integer that does not lead with zero
+      videoObject.episodeNumber = bcItem.custom_fields.ott_episode_number;
+    }else {
+      throw new ReferenceError("Episode number is not formatted correctly for video " + bcItem.id);
+    } 
+  }
+  videoObject.shortDescription = bcItem.description;
+  videoObject.longDescription = bcItem.long_description;
+  videoObject.ratings = {"rating": bcItem.custom_fields.ott_rating, "ratingSource": "CPR"} //Need to confirm if Telescope AgeRating field is appropriate
+  return videoObject;
+}
+  
+//Create Roku series object
+//Accepts a Brightcove video object and an array containing all Brightcove video objects
+//Retruns the Roku series object
+exports.createRokuSeries = (bcObject, bcItem) => {
+  let bcSeriesItem = bcObject.find((item) => {
+    if(bcItem.custom_fields.ott_type === "series with seasons") {
+      return item.custom_fields.ott_series_name === bcItem.custom_fields.ott_series_name && item.custom_fields.ott_season_number === "1" && item.custom_fields.ott_episode_number === "1";
+    }else{
+      return item.custom_fields.ott_series_name === bcItem.custom_fields.ott_series_name && item.custom_fields.ott_episode_number === "1";
+    }
+  })
+  if(bcSeriesItem === undefined) {
+    throw new ReferenceError("First episode for series not found for video "  + bcItem.id);
+  }
+  let seriesObject = {};
+  seriesObject.id = bcSeriesItem.custom_fields.ott_series_number;
+  seriesObject.releaseDate = bcSeriesItem.custom_fields.ott_release_date;
+  seriesObject.shortDescription = bcSeriesItem.custom_fields.ott_series_description;
+  seriesObject.tags = bcSeriesItem.custom_fields.ott_tags.trim().replace(/ *, */g, ",").split(","); //Trim whitespace and convert string to array
+  seriesObject.title = bcSeriesItem.custom_fields.ott_series_name;
+  seriesObject.genres = bcSeriesItem.custom_fields.ott_genres.trim().replace(/ *, */g, ",").split(","); //Trim whitespace and convert string to array
+  seriesObject.thumbnail = bcSeriesItem.images.thumbnail.src;
+  return seriesObject;
+}
