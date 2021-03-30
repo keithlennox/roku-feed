20a783bb962a73d96a7a0bb53181a5000b98f45f
klennox <klennox@tvo.org>
2021-03-30 15:44:21 -0400

    removed array containing multiple brightcove accounts

--- app.js
+++ app.js
@@ -2,16 +2,14 @@ const cron = require('node-cron');
 const { getBrightcoveVideos, getBrightcoveSource } = require('./scripts/brightcove');
 const { createRokuFeed, writeRokuFeed, writeLog} = require('./scripts/rokuFeed.js');
 
-const createRokuFeeds = async (accounts) => {
-  for (account of accounts) {
-    let bcVideos = await getBrightcoveVideos(account);
-    let bcSourcedVideos = await getBrightcoveSource(bcVideos);
-    let rokuFeed = createRokuFeed(bcSourcedVideos);
-    writeRokuFeed(rokuFeed.feed, account);
-    writeLog(account, bcVideos.length, rokuFeed.count);
-  }
+const createRokuFeeds = async (account) => {
+  let bcVideos = await getBrightcoveVideos(account);
+  let bcSourcedVideos = await getBrightcoveSource(bcVideos);
+  let rokuFeed = createRokuFeed(bcSourcedVideos);
+  writeRokuFeed(rokuFeed.feed, account);
+  writeLog(account, bcVideos.length, rokuFeed.count);
 }
 
-const accounts = [18140038001, 15364602001];
-
-createRokuFeeds(accounts); //A CRON job will trigger this
+//TVO = 18140038001
+//TVOKIDS = 15364602001
+createRokuFeeds(15364602001); //A CRON job will trigger this
