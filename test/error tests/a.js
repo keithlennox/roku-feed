fe7ac84b3eecff9099dbf7075756b888c7326d3b
klennox <klennox@tvo.org>
2021-03-30 15:46:06 -0400

    removed test files

--- test/error tests/a.js
+++
@@ -1,9 +0,0 @@
-const myFunction = () => {
-    const a = "hello";
-    const b = "goodbye";
-    const myVar = a + b;
-    console.log("alice cooper"); //Displays "alicecooper"
-    return myVar;
-}
-
-console.log(myFunction()); //Displays "hellogoodbye"
